9f670fc8d2c8326bf34991be9e5bd6840a2e3aa1
yanshou517 <ra89517r@gmail.com>
2024-11-12 20:31:57 +0800

    [更新]基本layout

--- vue-rabbit/src/views/Layout/index.vue
+++ vue-rabbit/src/views/Layout/index.vue
@@ -2,10 +2,17 @@
   <div>
     首頁
     <!-- 二級路由出口 -->
+    <LayoutNav />
+    <LayoutHeader />
     <RouterView />
+    <LayoutFooter />
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import LayoutNav from "./components/LayoutNav.vue";
+import LayoutHeader from "./components/LayoutHeader.vue";
+import LayoutFooter from "./components/LayoutFooter.vue";
+</script>
 
 <style lang="scss" scoped></style>
