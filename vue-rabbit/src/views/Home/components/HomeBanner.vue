f6f8d458eec4071dd0504addf0592b80be33942e
yanshou517 <ra89517r@gmail.com>
2024-11-13 00:32:37 +0800

    [更新]輪播圖 & 輪播圖api封裝

--- vue-rabbit/src/views/Home/components/HomeBanner.vue
+++ vue-rabbit/src/views/Home/components/HomeBanner.vue
@@ -1,7 +1,41 @@
+<script setup>
+import { ref, onMounted } from "vue";
+import { getBannerAPI } from "@/apis/home";
+
+const bannerList = ref([]);
+const getBanner = async () => {
+  const res = await getBannerAPI();
+  // console.log(res);
+  bannerList.value = res.result;
+};
+
+onMounted(() => {
+  getBanner();
+});
+</script>
+
 <template>
-  <div>Banner</div>
+  <div class="home-banner">
+    <el-carousel height="500px">
+      <el-carousel-item v-for="item in bannerList" :key="item">
+        <img :src="item.imgUrl" alt="" />
+      </el-carousel-item>
+    </el-carousel>
+  </div>
 </template>
 
-<script setup></script>
+<style scoped lang="scss">
+.home-banner {
+  width: 1240px;
+  height: 500px;
+  position: absolute;
+  left: 0;
+  top: 0;
+  z-index: 98;
 
-<style lang="scss" scoped></style>
+  img {
+    width: 100%;
+    height: 500px;
+  }
+}
+</style>
