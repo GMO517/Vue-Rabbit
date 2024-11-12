9e9dd8c875c0b13d792330b7be5c4dc9f0984e6e
yanshou517 <ra89517r@gmail.com>
2024-11-13 07:03:39 +0800

    [更新]篩選功能

--- vue-rabbit/src/views/SubCategory/index.vue
+++ vue-rabbit/src/views/SubCategory/index.vue
@@ -1,18 +1,43 @@
 <script setup>
 import { getCategoryFilterAPI } from "@/apis/category";
+import { getSubCategoryAPI } from "@/apis/category";
 import { ref, onMounted } from "vue";
 import { useRoute } from "vue-router";
 import { convertObjectToTC } from "@/utils/convertText";
+import GoodsItem from "../Home/components/GoodsItem.vue";
 
-//獲取麵包屑導航列數據
-const categoryData = ref([]);
+// 獲取麵包屑導航列數據
+const categoryData = ref({});
 const route = useRoute();
 const getCategoryData = async () => {
   const res = await getCategoryFilterAPI(route.params.id);
   categoryData.value = convertObjectToTC(res.result);
 };
+
+// 獲取基礎列表數據渲染
+const goodsList = ref([]);
+const reqData = ref({
+  categoryId: route.params.id,
+  page: 1,
+  pageSize: 20,
+  sortField: "publishTime",
+});
+
+const getGoodsList = async () => {
+  const res = await getSubCategoryAPI(reqData.value);
+  goodsList.value = convertObjectToTC(res.result.items);
+};
+
+// tab切換時callback
+const tabChange = () => {
+  // console.log("切換了", reqData.value.sortField);
+  reqData.value.page = 1; //記得要Reset一下
+  getGoodsList();
+};
+
 onMounted(() => {
   getCategoryData();
+  getGoodsList();
 });
 </script>
 
@@ -33,13 +58,14 @@ onMounted(() => {
       </el-breadcrumb>
     </div>
     <div class="sub-container">
-      <el-tabs>
+      <el-tabs v-model="reqData.sortField" @tab-change="tabChange">
         <el-tab-pane label="最新商品" name="publishTime"></el-tab-pane>
         <el-tab-pane label="最高人氣" name="orderNum"></el-tab-pane>
         <el-tab-pane label="評論最多" name="evaluateNum"></el-tab-pane>
       </el-tabs>
       <div class="body">
         <!-- 商品列表 -->
+        <GoodsItem v-for="goods in goodsList" :goods="goods" :key="goods.id" />
       </div>
     </div>
   </div>
