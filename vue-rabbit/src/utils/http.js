070a5a4845a3718829486e65d706a7d8caf3d04e
yanshou517 <ra89517r@gmail.com>
2024-11-15 06:31:39 +0800

    [更新]token攔截器攜帶

--- vue-rabbit/src/utils/http.js
+++ vue-rabbit/src/utils/http.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import "element-plus/theme-chalk/el-message.css";
 import { ElMessage } from "element-plus";
-
+import { useUserStore } from "@/stores/user";
 const httpInstance = axios.create({
   baseURL: "http://pcapi-xiaotuxian-front-devtest.itheima.net",
   timeout: 5000,
@@ -12,6 +12,14 @@ const httpInstance = axios.create({
 // axios請求攔截器
 httpInstance.interceptors.request.use(
   (config) => {
+    // 1.從pinia拿取token資料
+    const userStore = useUserStore();
+    // 2. 按照後端的請求要求拼接token資料
+    const token = userStore.userInfo.token;
+    if (token) {
+      // Authorization 受保護的資料 代表token不會洩漏
+      config.headers.Authorization = `Bearer ${token}`;
+    }
     return config;
   },
   (e) => Promise.reject(e)
