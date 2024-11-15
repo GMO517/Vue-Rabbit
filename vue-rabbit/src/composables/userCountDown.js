d5831b220edc1810859752ef95cd101313618ce0
yanshou517 <ra89517r@gmail.com>
2024-11-16 06:48:57 +0800

    [更新]修復build失敗

--- vue-rabbit/src/composables/userCountDown.js
+++ vue-rabbit/src/composables/userCountDown.js
@@ -1,31 +1,38 @@
 // 封裝倒數計時功能
-import { computed, onUnmounted, ref } from "vue";
-import dayjs from "dayjs";
+import { computed, ref } from "vue";
+import { useIntervalFn } from "@vueuse/core";
 
 export const useCountDown = () => {
   let timer = null;
   // 1.響應式資料
   const time = ref(0);
+
   // 格式化時間為 XX分OO秒
-  const formatTime = computed(() => dayjs.unix(time.value).format("mm分ss秒"));
+  const formatTime = computed(() => {
+    const minutes = Math.floor(time.value / 60)
+      .toString()
+      .padStart(2, "0"); // 計算分鐘並補齊 2 位數
+    const seconds = (time.value % 60).toString().padStart(2, "0"); // 計算秒數並補齊 2 位數
+    return `${minutes}分${seconds}秒`;
+  });
+
   // 2.開啟倒數的方法
   const start = (currentTime) => {
-    // 開始倒計時的邏輯
-    // 核心邏輯的編寫: 每隔一秒-1
+    // 透過 useIntervalFn 來實現倒數計時
+    time.value = currentTime;
 
-    // setInterval(callback, delay, ...args)
-    // 定時
-    timer = setInterval(() => {
-      currentTime -= 1;
-      time.value = currentTime;
+    // 每秒更新一次倒數時間
+    timer = useIntervalFn(() => {
+      if (time.value > 0) {
+        time.value -= 1;
+      } else {
+        timer.stop(); // 當倒數結束時停止計時器
+      }
     }, 1000);
+
+    timer.start(); // 啟動計時器
   };
 
-  // 組件銷毀時清除計時器
-  onUnmounted(() => {
-    timer && clearInterval(timer);
-  });
-  
   return {
     formatTime,
     start,
--- vue-rabbit/vite.config.js
+++ vue-rabbit/vite.config.js
@@ -7,6 +7,7 @@ import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
 import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 
+// commonjs 插件來處理不完全支持ESM的模塊
 import commonjs from "@rollup/plugin-commonjs";
 
 // https://vitejs.dev/config/
@@ -19,12 +20,16 @@ export default defineConfig({
     }),
     // 使用Components自動導入Vue組件，並且支持ElementPlus
     Components({
-      resolvers: [
-        // 1. 配置elementPlus採用sass樣式配色系統
-        ElementPlusResolver({ importStyle: "sass" }),
+      resolvers: [ElementPlusResolver({ importStyle: "sass" })],
+    }),
+    // 讓Vite處理CommonJS模塊
+    commonjs({
+      include: [
+        // 明確包含 dayjs 及其插件，避免 Rollup 處理錯誤
+        "node_modules/.pnpm/dayjs@1.11.4/node_modules/dayjs/**/*.js",
+        "node_modules/.pnpm/dayjs@1.11.4/node_modules/dayjs/plugin/**/*.js",
       ],
     }),
-    commonjs(), // 讓Vite處理CommonJS模塊
   ],
   resolve: {
     // 配置路徑別名，@ 指向 src 目錄
@@ -44,4 +49,11 @@ export default defineConfig({
     },
   },
   base: "/Vue-Rabbit/",
+  optimizeDeps: {
+    include: [
+      // 明確包含 dayjs 和 element-plus，提高性能和解決導入錯誤
+      "element-plus",
+      "dayjs",
+    ],
+  },
 });
