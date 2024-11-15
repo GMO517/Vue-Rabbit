82d7610ff71db299b7bdabc0b4338231fa90db39
yanshou517 <ra89517r@gmail.com>
2024-11-16 06:19:52 +0800

    [更新]訂單狀態調整 會員中心跟訂單查詢頁面跳轉

--- vue-rabbit/src/views/Layout/components/LayoutNav.vue
+++ vue-rabbit/src/views/Layout/components/LayoutNav.vue
@@ -39,8 +39,14 @@ const confirm = () => {
               </template>
             </el-popconfirm>
           </li>
-          <li><a href="javascript:;">我的訂單</a></li>
-          <li><a href="javascript:;">會員中心</a></li>
+          <li>
+            <a href="javascript:;" @click="router.push('/member/order')"
+              >我的訂單</a
+            >
+          </li>
+          <li>
+            <a href="javascript:;" @click="router.push('/member')">會員中心</a>
+          </li>
         </template>
         <template v-else>
           <li>
